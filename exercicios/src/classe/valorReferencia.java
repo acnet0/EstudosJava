62d1a8ba613467fd2162804874dec156bddabf70
acnet0 <ac.neto@outlook.com.br>
2025-05-21 00:05:04 -0300

    valorReferencia21-05

--- exercicios/src/classe/valorReferencia.java
+++ exercicios/src/classe/valorReferencia.java
@@ -20,5 +20,24 @@ public class valorReferencia {
 		
 		System.out.println(d1.obterDataFormatada());
 		System.out.println(d2.obterDataFormatada());
+		
+		voltarDataParaValorPadrao(d1);
+		
+		System.out.println(d1.obterDataFormatada());
+		System.out.println(d2.obterDataFormatada());
+		
+		int c =5;
+		alterarPrimitivo(c);
+		System.out.println(c);
+	}
+	
+	static void voltarDataParaValorPadrao(Data d) {
+		d.dia = 1;
+		d.mes = 1;
+		d.ano = 1970;	
+		}
+	
+	static void alterarPrimitivo(int c) {
+		c++;
 	}
 }
